946975c774ea690859674c47eb3815a35c92c1de
dariabushueva <bushueva84@yandex.ru>
2023-05-16 19:00:31 +0300

    changed item and test_item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,23 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+import pytest
+
+
+@pytest.fixture
+def item_1():
+    return Item("Смартфон", 10000, 20)
+
+
+def test_item_init(item_1):
+    assert item_1.name == "Смартфон"
+    assert item_1.price == 10000
+    assert item_1.quantity == 20
+
+
+def test_calculate_total_price(item_1):
+    assert item_1.calculate_total_price() == 200000
+
+
+def test_apply_discount(item_1):
+    item_1.pay_rate = 0.5
+    assert item_1.apply_discount() == 5000
+
