d05d29b62c1a2c7bfb9a735759ab2de7cff6198d
dariabushueva <bushueva84@yandex.ru>
2023-05-19 17:26:42 +0300

    changed item: name-private, added instantiate_from_csv, string_to_number; changed test_item added tests for new methods

--- src/item.py
+++ src/item.py
@@ -1,9 +1,14 @@
+import csv
+import os
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
     pay_rate = 1.0
     all = []
+    CSV_PATH = "C:\DARIA\Python_Projects\electronics_shop_project\src\items.csv"
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
@@ -13,11 +18,23 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
         Item.all.append(self)
+
+    @property
+    def name(self):
+        if len(self.__name) <= 10:
+            return self.__name
+        else:
+            raise Exception("Длина наименования товара превышает 10 символов.")
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
 
     def calculate_total_price(self) -> float:
         """
@@ -33,3 +50,23 @@ class Item:
         """
         self.price = self.price * self.pay_rate
         return self.price
+
+    @classmethod
+    def instantiate_from_csv(cls) -> None:
+        """
+        Инициализирует экземпляры класса Item данными из файла src/items.csv
+        """
+        cls.all.clear()
+        with open(cls.CSV_PATH, newline='') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                cls(row['name'], row['price'], row['quantity'])
+
+    @staticmethod
+    def string_to_number(string):
+        """
+        Возвращает число из числа-строки
+        """
+        return int(float(string))
+
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -21,3 +21,18 @@ def test_apply_discount(item_1):
     item_1.pay_rate = 0.5
     assert item_1.apply_discount() == 5000
 
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
+    item3 = Item.all[2]
+    assert item3.name == 'Кабель'
+    assert item3.price == '10'
+    assert item3.quantity == '5'
+
+
+def test_string_to_number():
+    assert Item.string_to_number('3') == 3
+    assert Item.string_to_number('3.0') == 3
+    assert Item.string_to_number('3.5') == 3
+
